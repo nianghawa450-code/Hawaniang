ddaa7edbfd477a4d4503ef5f4c77e119e5a96f02
nianghawa450-code <nianghawa450@gmail.com>
2026-04-25 17:06:56 +0000

    Add files via upload

---
+++ Portofolio.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+with st.sidebar:
+    st.title("HAWA NIANG")
+    st.header("TECHNICIENNE SUPERIEUR EN GEOMATIQUE")
+    st.markdown("""
+    * Email: nianghawa450@gmail.com
+    * Adresse: Ouakam/Dakar/Sénégal
+    """)
+st.subheader("PROFIL PROFESSIONNEL")
+
+st.write("Technicienne en géomatique et experte en amenagement urbain, je possède une double compétence technique et stratégique. je maitrise l'ensemble de la chaine de prduction cartographique de la collecte de données sur le terrain à l'analyse pour la planification des villes. Mon objectif est d'utiliser les outils numériques pour améliorer la gestion du territoire et des services urbains en Afrique. ")    
+st.divider()    
+    
+
+st.subheader("COMPETENCES")
+st.markdown("""
+* Modélisation de bases de données relationnelles et conceptuelles sur PowerAMC 
+* Maitrise avancée de QGIS et ARCGIS pour la réalisation de carte et pour de l'analyse de l'espace
+* Réalisation de plans de maison, de lotissement, voirie et réseaux divers sur Autocad
+* conception 3D de batiments et projets urbains avec SKetchUp 
+* analyste territoriale et diagnostic urbain
+* Participation à la mise en place de projets d'aménagement
+""")
+st.divider()
+st.subheader("FORMATIONS")
+st.markdown("""
+* 2021-2022: Baccalaureat 
+* 2024-2025: Licence en Geographie à l'Université Cheikh Anta Diop de Dakar
+* 2025-2026: Brevet de Technicien Superieur en Geomatique 
+* 2025-2026: Master en Amenagement des Gestions Urbaines en Afrique
+""")
